4ba847d13a30ba36c4a8af01174a6c92aad24af9
anji-automation <anjaneyulu.by6@gmail.com>
2024-07-29 17:00:54 +0530

    PR suggest for batch changed with 89,331

--- test/src/main/java/com/veracode/verademo/commands/IgnoreCommand.java
+++ test/src/main/java/com/veracode/verademo/commands/IgnoreCommand.java
@@ -47,7 +47,7 @@ public class IgnoreCommand implements BlabberCommand {
 			sqlStatement.execute(sqlQuery);
 			/* END EXAMPLE VULNERABILITY */
 		} catch (SQLException e) {
-			// TODO Auto-generated catch blockeddd
+			// TODO Auto-generated catch blockedd
 			e.printStackTrace();
 		}
 	}
