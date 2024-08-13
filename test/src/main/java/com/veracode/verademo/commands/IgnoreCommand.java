2301bc63f19dd40429b7f6efe3009c1fcd2621d0
anji-automation <anjaneyulu.by6@gmail.com>
2024-08-13 11:06:49 +0530

    Code push from automation

--- test/src/main/java/com/veracode/verademo/commands/IgnoreCommand.java
+++ test/src/main/java/com/veracode/verademo/commands/IgnoreCommand.java
@@ -69,3 +69,5 @@ public class IgnoreCommand implements BlabberCommand {
 // Modified at 2024-08-12T18:29:42.643Z
 
 // Modified at 2024-08-12T18:29:47.462Z
+
+// Modified at 2024-08-13T05:36:49.701Z
